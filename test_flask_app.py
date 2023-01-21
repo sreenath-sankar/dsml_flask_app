fd1e149de1418d3e3b1b7471d8f1c20a675f87eb
sreenath-sankar <sreenathsankarssr@gmail.com>
2023-01-22 01:02:16 +0530

    Update test_flask_app.py

--- test_flask_app.py
+++ test_flask_app.py
@@ -8,7 +8,7 @@ def client():
 def test_ping(client):
     resp = client.get('/ping')
     assert resp.status_code == 200
-    assert resp.json == {"message": "Hi there, I'm working with new API!!"}
+    assert resp.json == {"message": "Hi there, I'm working with new API :)!!"}
 
 def test_predict(client):
     test_data = {'Gender':"Male", 
